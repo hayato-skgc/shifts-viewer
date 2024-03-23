d681346dacc02063aedc078cb2defdeea4aa9275
Hayato Sakaguchi <hayato8190+univ@gmail.com>
2024-03-24 02:04:42 +0900

    add font-family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,10 +22,25 @@ import { CssBaseline } from '@mui/material';
 import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
+import { M_PLUS_1p } from 'next/font/google';
+
 import { Provider } from 'jotai';
+
+const mplus = M_PLUS_1p({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export const theme = createTheme({
   typography: {
+    fontFamily: [
+      `"${mplus.style.fontFamily}"`,
+      '"メイリオ"',
+      'Meiryo',
+      '"ヒラギノ角ゴシック"',
+      '"Hiragino Sans"',
+      'sans-serif'
+    ].join(','),
     button: {
       textTransform: 'none'
     }
